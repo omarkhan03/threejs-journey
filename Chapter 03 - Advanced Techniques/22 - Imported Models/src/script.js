ef02f1639dbf9bbb0b9c43bf994d1b0375ef562e
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-02-21 00:12:11 -0700

    Edges + floor selector on ICT

--- Chapter 03 - Advanced Techniques/22 - Imported Models/src/script.js
+++ Chapter 03 - Advanced Techniques/22 - Imported Models/src/script.js
@@ -2,8 +2,17 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js'
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
 
 import * as dat from 'lil-gui'
+import { BufferGeometry } from 'three'
+
+const floor5 = document.getElementsByClassName('f5')[0]
+const floor4 = document.getElementsByClassName('f4')[0]
+const floor3 = document.getElementsByClassName('f3')[0]
+const floor2 = document.getElementsByClassName('f2')[0]
+const floor1 = document.getElementsByClassName('f1')[0]
+
 
 /**
  * Base
@@ -17,62 +26,60 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// /**
-//  * Duck
-//  */
-// const gltfLoader = new GLTFLoader()
-
-// gltfLoader.load(
-//     '/models/Duck/glTF/Duck.gltf',
-//     (gltf) => {
-//         scene.add(gltf.scene.children[0])
-//     }
-// )
-
-/**
- * Helmet
- */
-// const gltfLoader = new GLTFLoader()
-
-// gltfLoader.load(
-//     '/models/ICT/ICT.gltf',
-//     (gltf) => {
-
-//         console.log(gltf)
-
-//         // while(gltf.scene.children.length) {
-//         //     scene.add(gltf.scene.children[0])
-//         // }
-
-//         // scene.add(gltf.scene.children[0])
-//         // const children = [...gltf.scene.children]
-//         // for(const child of children) {
-//         //     scene.add(child)
-//         // }
-
-//         scene.add(gltf.scene)
-//     }
-// )
-
+// Loading ICT model 
 var loader = new ColladaLoader();
 loader.load("/models/ICT/ICT_COLLADA.dae", function (result) {
+
+    const meshes = result.scene.children[0].children
+
+    for(const mesh of meshes) {
+        const geometry = mesh.geometry
+        console.log(geometry)
+        const edges = new THREE.EdgesGeometry(geometry)
+        const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 100 }))
+        result.scene.add(line);
+    }
+
     scene.add(result.scene);
 });
 
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
-)
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
+// Selected floor
+const geometry = new THREE.BoxGeometry(130,5,70)
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, opacity: 0.2, transparent: true})
+const selected = new THREE.Mesh(geometry, material)
+selected.position.set(-11, 9, -8)
+scene.add(selected)
+selected.visible = false
+
+floor5.addEventListener('click', () => {
+    console.log('f5')
+    selected.visible = true
+    selected.position.set(-11, 30, -8)
+})
+
+floor4.addEventListener('click', () => {
+    console.log('f4')
+    selected.visible = true
+    selected.position.set(-11, 23, -8)
+})
+
+floor3.addEventListener('click', () => {
+    console.log('f3')
+    selected.visible = true
+    selected.position.set(-11, 16, -8)
+})
+
+floor2.addEventListener('click', () => {
+    console.log('f2')
+    selected.visible = true
+    selected.position.set(-11, 9, -8)
+})
+
+floor1.addEventListener('click', () => {
+    console.log('f1')
+    selected.visible = true
+    selected.position.set(-11, 2, -8)
+})
 
 /**
  * Lights
@@ -118,9 +125,11 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 2000)
-camera.position.set(-96, 34, 63)
-camera.rotation.set(-0.3, -0.7, 63)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 3000)
+camera.position.set(-78.6, 32, 45)
+
+camera.rotation.set(-0.06, -0.76, 0.04)
+
 scene.add(camera)
 
 // Controls
@@ -147,12 +156,11 @@ let previousTime = 0
 
 const tick = () =>
 {
-    console.log(camera.position)
-    console.log(camera.rotation)
-
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    // console.log(camera.position, camera.rotation)
 
     // Update controls
     controls.update()
